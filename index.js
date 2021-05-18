b84071aba4467e1c9bb2efa71944ff4da34f2422
Julien Rousseau <julienrousseau16@gmail.com>
2021-05-18 23:18:55 +0200

    first route to get 1 item is functional

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const connection = require("./db-config");
-const express = require('express')
-const app = express()
+// const connection = require("./db-config");
+// const cors = require('cors')
+// const express = require('express')
 
+// const app = express()
+// app.use(cors())
+
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const connection = require('./db-config')
+const cors = require('cors')
 const port = process.env.PORT || 5555
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
 
 app.get('/items', (req, res) => {
   connection.query('SELECT * FROM items', (err, result) => {
